9d9e193d9ff3785b4e333fd99cc2ee1652098e36
AntonUkhov <89222229263@mail.ru>
2021-08-16 19:15:46 +0500

    added templates

--- DJsite/women/views.py
+++ DJsite/women/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import *
+
+menu = ['О сайте', "Добавить статью", "Обратная связь", "Войти"]
+
 
 def index(request):
-    return HttpResponse('Женская страница')
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О нас'})
+
 
 def categories(request, catid):
     if request.POST:
         print(request.POST)
     return HttpResponse(f'<h1>Стати по категориям</h1><p>{catid}</p>')
 
+
 def archive(request, year):
     if int(year) > 2021:
         return redirect('home', permanent=True)
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</p>')
