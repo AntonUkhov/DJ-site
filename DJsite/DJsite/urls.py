813494b286bcf5030ca06c18a2e9a85db9734181
AntonUkhov <89222229263@mail.ru>
2021-08-16 18:27:03 +0500

    added models Women

--- DJsite/DJsite/urls.py
+++ DJsite/DJsite/urls.py
@@ -1,8 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from women.views import *
+from DJsite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', include('women.urls'))
+    path('', include('women.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
